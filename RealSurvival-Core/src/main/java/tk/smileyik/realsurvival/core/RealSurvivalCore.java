a1d5a36c7cf24aae4a1762ee5e0cdc5f529f5f9f
Smile Yik <miskyle@outlook.com>
2022-05-23 23:30:31 +0800

    重新构建项目目录

---
+++ RealSurvival-Core/src/main/java/tk/smileyik/realsurvival/core/RealSurvivalCore.java
@@ -0,0 +1,17 @@
+package tk.smileyik.realsurvival.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RealSurvivalCore extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
